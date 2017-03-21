35f1e0a4cb330b1581bceae3212e2a5c1740820f
poschengband <ckousky@gmail.com>
2017-03-21 17:38:45 -0500

    elliposchengband (3be7781): compromise ... show the native spell frequency when using easy_lore so long as no spells have been observed

--- src/mon_display.c
+++ src/mon_display.c
@@ -389,7 +389,7 @@ static void _display_resists(monster_race *r_ptr, doc_ptr doc)
 static void _display_frequency(monster_race *r_ptr, doc_ptr doc)
 {
     int pct = 0;
-    if (spoiler_hack)
+    if (spoiler_hack || (!r_ptr->r_spell_turns && _easy_lore(r_ptr)))
         pct = r_ptr->freq_spell * 100;
     else if (r_ptr->r_spell_turns)
     {
@@ -401,7 +401,7 @@ static void _display_frequency(monster_race *r_ptr, doc_ptr doc)
         vec_ptr v = vec_alloc((vec_free_f)string_free);
 
         doc_printf(doc, "Spells  : <indent><color:G>%d.%02d%%</color> ", pct/100, pct%100);
-        if (!spoiler_hack)
+        if (!spoiler_hack && r_ptr->r_spell_turns + r_ptr->r_move_turns > 0)
             doc_printf(doc, "(%d of %d moves) ", r_ptr->r_spell_turns, r_ptr->r_spell_turns + r_ptr->r_move_turns);
 
         if (r_ptr->flags2 & RF2_SMART)
