3e4cd92c705cc9d5799a2507fc0b5b12d1bec5d4
poschengband <ckousky@gmail.com>
2017-04-03 18:01:27 -0500

    mingw: support building windows from linux
    To make this work, I had to borrow some z-files.c utilities from
    Vanilla Angband ... What would I ever do without those guys :D
    
    The initial build using:
      ./autogen.sh
      ./configure --enable-win --disable-curses --build=i686-pc-linux-gnu --host=i586-mingw32msvc
    worked like a champ, once I managed to grab the correct file
    (poschengband.exe rather than poschengband ... sigh). Probably, I
    need to spend some time testing on Windows to make sure there are
    no surprises.

--- src/h-basic.h
+++ src/h-basic.h
@@ -15,6 +15,28 @@
 #include "autoconf.h"
 #endif /* HAVE_CONFIG_H */
 
+/**
+ * Every system seems to use its own symbol as a path separator.
+ *
+ * Default to the standard Unix slash, but attempt to change this
+ * for various other systems.  Note that any system that uses the
+ * "period" as a separator (i.e. RISCOS) will have to pretend that
+ * it uses the slash, and do its own mapping of period <-> slash.
+ *
+ * It is most definitely wrong to have such things here.  Platform-specific
+ * code should handle shifting Angband filenames to platform ones. XXX
+ */
+#undef PATH_SEP
+#define PATH_SEP "/"
+#define PATH_SEPC '/'
+
+#ifdef WINDOWS
+# undef PATH_SEP
+# undef PATH_SEPC
+# define PATH_SEP "\\"
+# define PATH_SEPC '\\'
+#endif
+
 /* System Configuration */
 #include "h-config.h"
 
