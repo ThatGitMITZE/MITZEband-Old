floatRand/PoschengbandR

Don't reduce weaponsmith essence gain due to removable curses.
It isn't very much fun to carry around a bunch of remove curse and equip
and uncurse every cursed ego item you find before destroying it.

src/weaponsmith.c
@@ -432,7 +432,7 @@ static void _absorb_all(object_type *o_ptr, _absorb_essence_f absorb_f)
     object_flags(&new_obj, new_flgs);
 
     /* Ammo and Curses */
-    if (o_ptr->curse_flags & (TRC_CURSED | TRC_HEAVY_CURSE | TRC_PERMA_CURSE)) div++;
+    if (o_ptr->curse_flags & TRC_PERMA_CURSE) div++;
     if (have_flag(old_flgs, TR_AGGRAVATE)) div++;
     if (have_flag(old_flgs, TR_NO_TELE)) div++;
     if (have_flag(old_flgs, TR_DRAIN_EXP)) div++;

ThatGitMITZE/MITZEband-Old

(cherry picked from commit 6a863d7c76de2f49b59893e404ecdf7c907a257a)
Conflicts:
	src/weaponsmith.c

src/weaponsmith.c
@@ -431,7 +431,7 @@ static void _absorb_all(object_type *o_ptr, _absorb_essence_f absorb_f)
     obj_flags(&new_obj, new_flgs);
 
     /* Ammo and Curses */
-    if (o_ptr->curse_flags & (OFC_CURSED | OFC_HEAVY_CURSE | OFC_PERMA_CURSE)) div++;
+    if (o_ptr->curse_flags & OFC_PERMA_CURSE) div++;
     if (have_flag(old_flgs, OF_AGGRAVATE)) div++;
     if (have_flag(old_flgs, OF_NO_TELE)) div++;
     if (have_flag(old_flgs, OF_DRAIN_EXP)) div++;