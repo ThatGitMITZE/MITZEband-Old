09a02ae9973115d5a85d26597b19f0ae9d6163a2
poschengband <ckousky@gmail.com>
2017-04-20 17:03:43 -0500

    Spoilers and Helpfiles: Regenerate

--- src/spoilers.c
+++ src/spoilers.c
@@ -1591,7 +1591,7 @@ static void _skills_race_table(FILE* fp)
 static void _skills_class_table(FILE* fp)
 {
     int i,j;
-    fputs("Class,Dis,Dev,Sav,Stl,Srh,Fos,Thn,Thb,Dis2,Dev2,Sav2,Thn2,Thb2,Riding,DualWielding\n", fp);
+    fputs("Class,Dis,Dev,Sav,Stl,Srh,Fos,Thn,Thb,Dis2,Dev2,Sav2,Thn2,Thb2,Life,BHP,Riding,DualWielding\n", fp);
     for (i = 0; i < MAX_CLASS; i++)
     {
         int max_j = 1;
@@ -1611,7 +1611,7 @@ static void _skills_class_table(FILE* fp)
                 fprintf(fp, "\"%s:%s\",", class_ptr->name, class_ptr->subname);
             else
                 fprintf(fp, "\"%s\",", class_ptr->name);
-            fprintf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d+%d,%d+%d,%d+%d,%d+%d,%d+%d,%d,%d\n",
+            fprintf(fp, "%d,%d,%d,%d,%d,%d,%d,%d,%d+%d,%d+%d,%d+%d,%d+%d,%d+%d,%d,%d,%d,%d\n",
                 class_ptr->base_skills.dis + 5*class_ptr->extra_skills.dis,
                 class_ptr->base_skills.dev + 5*class_ptr->extra_skills.dev,
                 class_ptr->base_skills.sav + 5*class_ptr->extra_skills.sav,
@@ -1625,6 +1625,7 @@ static void _skills_class_table(FILE* fp)
                 class_ptr->base_skills.sav, class_ptr->extra_skills.sav,
                 class_ptr->base_skills.thn, class_ptr->extra_skills.thn,
                 class_ptr->base_skills.thb, class_ptr->extra_skills.thb,
+                class_ptr->life, class_ptr->base_hp,
                 s_info[i].s_max[SKILL_RIDING],
                 s_info[i].s_max[SKILL_DUAL_WIELDING]
             );
