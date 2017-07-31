5f342fed1be7eca52f9cb665ea9da3cd43d2fabb
poschengband <ckousky@gmail.com>
2017-07-31 07:35:52 -0500

    Retaliation: Paralyzed monsters should not retaliate

--- src/cmd1.c
+++ src/cmd1.c
@@ -2018,7 +2018,11 @@ void touch_zap_player(int m_idx)
     if (p_ptr->prace == RACE_MON_BEHOLDER || p_ptr->lightning_reflexes)
         return;
 
-    if ((r_ptr->flags2 & RF2_AURA_REVENGE) && !retaliation_hack && !MON_CONFUSED(m_ptr) && randint0(150) < r_ptr->level)
+    if ( (r_ptr->flags2 & RF2_AURA_REVENGE)
+      && !retaliation_hack
+      && !MON_CONFUSED(m_ptr)
+      && !MON_PARALYZED(m_ptr)
+      && randint0(150) < r_ptr->level )
     {
         retaliation_hack = TRUE;
         make_attack_normal(m_idx);
