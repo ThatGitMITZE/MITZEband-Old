0beaa33fe6e98693486bf0440944cb8730712931
sulkasormi <37194102+sulkasormi@users.noreply.github.com>
2018-03-16 22:10:36 +0200

    use max exp instead of cur exp for oook and scores
    * use max exp instead of cur exp for oook and scores
    
    * use max exp instead of cur exp for oook and scores

--- src/py_display.c
+++ src/py_display.c
@@ -2358,10 +2358,10 @@ static void _add_html_header(doc_ptr doc)
     string_printf(header,  " <meta name='race' value='%s'>\n", get_race()->name);
     string_printf(header,  " <meta name='class' value='%s'>\n", get_class()->name);
     string_printf(header,  " <meta name='level' value='%d'>\n", p_ptr->lev);
-    string_printf(header,  " <meta name='experience' value='%d'>\n", p_ptr->exp);
+    string_printf(header,  " <meta name='experience' value='%d'>\n", p_ptr->max_exp);
     string_printf(header,  " <meta name='turncount' value='%d'>\n", game_turn);
     string_printf(header,  " <meta name='max_depth' value='%d'>\n", _max_depth());
-    string_printf(header,  " <meta name='score' value='%d'>\n", p_ptr->exp); /* ?? Does oook need this? */
+    string_printf(header,  " <meta name='score' value='%d'>\n", p_ptr->max_exp); /* ?? Does oook need this? */
     string_printf(header,  " <meta name='fame' value='%d'>\n", p_ptr->fame);
 
     /* For angband.oook.cz ... I'm not sure what is best for proper display of html dumps so I'll need to ask pav
