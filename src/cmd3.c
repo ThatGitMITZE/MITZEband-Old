ec9642a4546658a64d2b93f273f999114eedc388
sulkasormi <37194102+sulkasormi@users.noreply.github.com>
2018-05-03 01:27:12 +0300

    stealing Gwarl's addition of buildings to object list

--- src/cmd3.c
+++ src/cmd3.c
@@ -1686,7 +1686,7 @@ static _obj_list_ptr _create_obj_list(void)
             {
                 cave_type *c_ptr = &cave[y][x];
                 feature_type *f_ptr = &f_info[c_ptr->feat];
-                if (have_flag(f_ptr->flags, FF_STORE) || have_flag(f_ptr->flags, FF_STAIRS))
+                if (have_flag(f_ptr->flags, FF_STORE) || have_flag(f_ptr->flags, FF_STAIRS) || have_flag(f_ptr->flags, FF_BLDG))
                 {
                     _obj_list_info_ptr info = _obj_list_info_alloc();
                     info->group = _GROUP_FEATURE;
@@ -1840,7 +1840,11 @@ static int _draw_obj_list(_obj_list_ptr list, int top, rect_t rect)
                     (info_ptr->dy > 0) ? 'S' : 'N', abs(info_ptr->dy),
                     (info_ptr->dx > 0) ? 'E' : 'W', abs(info_ptr->dx));
 
-            sprintf(name, "%s", f_name + f_ptr->name);
+            if (have_flag(f_ptr->flags, FF_BLDG) && !dun_level)
+            {
+                sprintf(name, "%s", building[f_ptr->subtype].name);
+            }
+            else sprintf(name, "%s", f_name + f_ptr->name);
             if (have_flag(f_ptr->flags, FF_QUEST_ENTER))
             {
                 int quest_id = cave[info_ptr->y][info_ptr->x].special;
