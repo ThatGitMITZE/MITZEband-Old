8ce38fd8c92ff71507e3a5fe62f0812d183e25b9
sulkasormi <37194102+sulkasormi@users.noreply.github.com>
2019-04-20 15:08:16 +0300

    avoid division by zero

--- src/karrot.c
+++ src/karrot.c
@@ -807,7 +807,8 @@ static int _karrot_happy_score(void)
             pisteet -= (20 * hudit);
         }
     }
-    rspct = MIN(MIN(120 + (10 * osumat), 180 + osumat), (pisteet * 100 / ptaso)) + (osumat * mult) - (hudit * MIN(4, hudit)) + (MAX(p_ptr->max_plv, osumat * 5 / 3) * 2);
+    if ((!ptaso) || (!osumat)) return 125;
+    else rspct = MIN(MIN(120 + (10 * osumat), 180 + osumat), (pisteet * 100 / ptaso)) + (osumat * mult) - (hudit * MIN(4, hudit)) + (MAX(p_ptr->max_plv, osumat * 5 / 3) * 2);
     if (osumat == MAX_KT_QUEST) rspct = MAX(rspct, 311); /* paranoia */
     return MAX(1, rspct);
 }
