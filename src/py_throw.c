41b8655155863e421bfa627a8870f8ef740731a4
sulkasormi <37194102+sulkasormi@users.noreply.github.com>
2019-11-09 23:22:10 +0200

    allow throwing ammo from the quiver

--- src/py_throw.c
+++ src/py_throw.c
@@ -69,7 +69,8 @@ static obj_ptr _get_obj(int type)
     {
         prompt.where[0] = INV_PACK;
         prompt.where[1] = INV_EQUIP;
-        prompt.where[2] = INV_FLOOR;
+        prompt.where[2] = INV_QUIVER;
+        prompt.where[3] = INV_FLOOR;
     }
     obj_prompt(&prompt);
     return prompt.obj;
